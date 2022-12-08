b0730bb911921eab6b6d8b188b083851e3f621f1
Jim <jimkk159@gmail.com>
2022-12-08 20:18:06 +0800

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// CSS
+
+//Custom component
+import Card from "./shared/components/UI/Card";
+
+function App() {
+  return (
+    <div>
+      <Card>
+        <h1>Hello world</h1>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
