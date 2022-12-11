49ead2794de47b67165e16df26ddddeeb94fa15a
Jim <jimkk159@gmail.com>
2022-12-12 01:16:48 +0800

    feat:add input touch & blur behavior

--- frontend/src/shared/components/Form/Input.jsx
+++ frontend/src/shared/components/Form/Input.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 
 import classes from "./Input.module.css";
 
+// const inputReducer = (state, action) => {
+//   switch (action.type) {
+//   }
+// };
+
 function Input(props) {
-    console.log(classes["input-control"]);
+  //   const [inputState, dispatch] = useReducer(inputReducer, {
+  //     value: "",
+  //     isTouched: false, //Check the user touch the input or not
+  //     isValid: false, //Check the the input is valid after user has touched
+  //   });
+  const [value, setValue] = useState("");
+  const [isToudched, setIsTouched] = useState(false);
+  const [isValid, setIsValid] = useState(false);
+  const changeHandler = (event) => {
+    setValue(event.target.value);
+    if (event.target.value.length >= 6) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  };
+  const blurHandler = () => {
+    setIsTouched(true);
+  };
   return (
-    <div className={`${classes["input-control"]} ${classes["input-invalid"]}`}>
+    <div className={`${classes["input-control"]} ${!isValid && isToudched && classes["input-invalid"]}`}>
       <label htmlFor={props.id}>{props.label}</label>
-      <input id={props.id} type={props.type} />
-      <p>{props.errorMessage}</p>
+      <input
+        id={props.id}
+        type={props.type}
+        onChange={changeHandler}
+        onBlur={blurHandler}
+        value={value}
+      />
+      {!isValid && isToudched && <p>{props.errorMessage}</p>}
     </div>
   );
 }
--- frontend/src/users/pages/AuthPage.jsx
+++ frontend/src/users/pages/AuthPage.jsx
@@ -14,31 +14,37 @@ import Button from "../../shared/components/Form/Button";
 function AuthPage() {
   const auth = useContext(AuthContext);
 
+  const authSubmitHandler = () => {};
   return (
     <>
       <Card className={classes.authentication}>
         <h1>Login Required</h1>
         <hr />
-        <form>
+        <form onSubmit={authSubmitHandler}>
           <Input
+          key="username"
             id="username"
             type="text"
             label="Name"
             errorMessage="Please enter your name."
           />
           <Input
+          key="email"
             id="email"
             type="text"
             label="E-Mail"
             errorMessage="Please enter a valid email."
           />
           <Input
-            id="email"
+           key="password"
+            id="password"
             type="password"
             label="Password"
             errorMessage="Enter at least 6 characters."
           />
-          <Button type="submit" disabled={false}>Login</Button>
+          <Button type="submit" disabled={false}>
+            Login
+          </Button>
         </form>
       </Card>
     </>
