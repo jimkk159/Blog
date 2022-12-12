74183569844192473e3afab5aac91979880c62d9
Jim <jimkk159@gmail.com>
2022-12-13 01:02:26 +0800

    fix:useMediaQuery bug

--- frontend/src/shared/hooks/media-query-hook.js
+++ frontend/src/shared/hooks/media-query-hook.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 // Check the Screen meet the media query
 // According to https://stackoverflow.com/questions/54491645/media-query-syntax-for-reactjs
@@ -10,7 +10,7 @@ function useMediaQuery(type, screenSize) {
     window
       .matchMedia(`(${type}-width:${screenSize}px)`)
       .addEventListener("change", (e) => setMatches(e.matches));
-  }, []);
+  }, [type, screenSize]);
 
   return { matches };
 }
