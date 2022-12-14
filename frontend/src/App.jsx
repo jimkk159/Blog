ac72893c7ade1dc722657f605030685341541712
Jim <jimkk159@gmail.com>
2022-12-14 09:46:36 +0800

    feat:split route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,14 @@
 import React from "react";
-import {
-  Route,
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
 //Context
 import AuthContextProvider from "./shared/context/auth-contex";
 
-//PAGE
-import RootLayout from "./shared/pages/layouts/RootLayout";
-import BlogLayout from "./shared/pages/layouts/RootLayout";
-import HomePage from "./shared/pages/HomePage";
-import AboutPage from "./shared/pages/AboutPage";
-import ErrorPage from "./shared/pages/ErrorPage";
-import AuthPage from "./users/pages/AuthPage";
-import NewPostPage from "./blogs/pages/NewPostPage";
-import PostPage from "./blogs/pages/PostPage";
-import PostsPage from "./blogs/pages/PostsPage";
+//Route
+import router from "./route/route";
 
 //CSS
 import "./App.module.css";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
-      <Route index element={<HomePage />} loader={null} />
-      <Route path="/blog" element={<BlogLayout />} loader={null}>
-        <Route index element={<PostsPage />} loader={null} />
-        <Route index path="new-blog" element={<NewPostPage />} loader={null} />
-        <Route path=":blogId" element={<PostPage />} loader={null} />
-      </Route>
-      <Route path="/about" element={<AboutPage />} loader={null} />
-      <Route path="/auth" element={<AuthPage />} loader={null} />
-      <Route path="/*" element={<Navigate replace to="/" />} loader={null} />
-    </Route>
-  )
-);
 
 function App() {
   return (
---
+++ frontend/src/route/route.jsx
@@ -0,0 +1,35 @@
+import {
+  Route,
+  Navigate,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+//PAGE
+import RootLayout from "../shared/pages/layouts/RootLayout";
+import BlogLayout from "../shared/pages/layouts/RootLayout";
+import HomePage from "../shared/pages/HomePage";
+import AboutPage from "../shared/pages/AboutPage";
+import ErrorPage from "../shared/pages/ErrorPage";
+import AuthPage from "../users/pages/AuthPage";
+import NewPostPage from "../blogs/pages/NewPostPage";
+import PostPage from "../blogs/pages/PostPage";
+import PostsPage from "../blogs/pages/PostsPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
+      <Route index element={<HomePage />} loader={null} />
+      <Route path="/blog" element={<BlogLayout />} loader={null}>
+        <Route index element={<PostsPage />} loader={null} />
+        <Route index path="new-blog" element={<NewPostPage />} loader={null} />
+        <Route path=":blogId" element={<PostPage />} loader={null} />
+      </Route>
+      <Route path="/about" element={<AboutPage />} loader={null} />
+      <Route path="/auth" element={<AuthPage />} loader={null} />
+      <Route path="/*" element={<Navigate replace to="/" />} loader={null} />
+    </Route>
+  )
+);
+
+export default router;
