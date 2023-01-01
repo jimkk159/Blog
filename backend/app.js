ded0f7037aa94bb4a6dd3300b6fe327dcdf980c1
Jim <jimkk159@gmail.com>
2023-01-01 21:13:24 +0800

    feat:add http error handle

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+//Models
+import HttpError from "./models/http-error.js";
+
 //Routes
 import postRouters from "./routes/post-route.js";
 
@@ -23,6 +26,18 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/posts", postRouters);
+
+//Undefined Route Case
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route", 404);
+  throw error;
+});
+
+//Deal with Error
+app.use((error, req, res, next) => {
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
 
 const port = 5000;
 app.listen(port, () => console.log("Server running on port 5000"));
--- backend/controllers/posts-controller.js
+++ backend/controllers/posts-controller.js
@@ -1,13 +1,23 @@
 import { DUMMY_Structure, Dummy_blogs, Dummy_search } from "./Dummy_data.js";
+let count = 0;
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+export const getAllPosts = async (req, res, next) => {
 
-export const getAllPosts = (req, res, next) => {
+  //For Debug
+  count += 1;
+  console.log("Get All Posts " + count);
+  await sleep(3000);
   res.json(Dummy_blogs);
 };
 
-export const getPostsStructure = (req, res, next) => {
+export const getPostsStructure = async (req, res, next) => {
+  //For Debug
+  console.log("Get Posts Structure");
   res.json(DUMMY_Structure);
 };
 
-export const getPostSearch = (req, res, next) => {
+export const getPostSearch = async (req, res, next) => {
+  //For Debug
+  console.log("Get Post Search");
   res.json(Dummy_search);
 };
