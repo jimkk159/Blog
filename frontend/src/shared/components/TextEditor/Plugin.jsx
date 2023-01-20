cea87e222b50f9cb3ed2460a842db3463397fb8f
Jim <jimkk159@gmail.com>
2023-01-20 20:55:20 +0800

    fix: drag and drop

--- frontend/src/shared/components/TextEditor/Plugin.jsx
+++ frontend/src/shared/components/TextEditor/Plugin.jsx
@@ -2,7 +2,7 @@ import { composeDecorators } from "@draft-js-plugins/editor";
 import createFocusPlugin from "@draft-js-plugins/focus";
 import createImagePlugin from "@draft-js-plugins/image";
 import createAlignmentPlugin from "@draft-js-plugins/alignment";
-import createBlockDndPlugin from '@draft-js-plugins/drag-n-drop';
+import createBlockDndPlugin from "@draft-js-plugins/drag-n-drop";
 import createResizeablePlugin from "@draft-js-plugins/resizeable";
 
 //CSS
@@ -15,7 +15,6 @@ const resizeablePlugin = createResizeablePlugin({ vertical: "relative" });
 const blockDndPlugin = createBlockDndPlugin();
 const alignmentPlugin = createAlignmentPlugin();
 
-
 const decorator = composeDecorators(
   resizeablePlugin.decorator,
   alignmentPlugin.decorator,
@@ -23,12 +22,38 @@ const decorator = composeDecorators(
   blockDndPlugin.decorator
 );
 
+const droppableBlockDndPlugin = {
+  ...blockDndPlugin,
+  handleDrop: (
+    selection,
+    dataTransfer,
+    isInternal,
+    { getEditorState, setEditorState }
+  ) => {
+    console.log(123)
+    const raw = dataTransfer.data.getData("text");
+    console.log(raw);
+
+    return blockDndPlugin.handleDrop(selection, dataTransfer, isInternal, {
+      getEditorState,
+      setEditorState,
+    });
+  },
+};
+
 const imagePlugin = createImagePlugin({
   decorator,
   theme: { image: classes["image-plugin"] },
 });
 
-const plugins = [blockDndPlugin, focusPlugin, alignmentPlugin, resizeablePlugin, imagePlugin];
+const plugins = [
+  blockDndPlugin,
+  droppableBlockDndPlugin,
+  focusPlugin,
+  alignmentPlugin,
+  resizeablePlugin,
+  imagePlugin,
+];
 
-export default plugins
+export default plugins;
 export const { AlignmentTool } = alignmentPlugin;
