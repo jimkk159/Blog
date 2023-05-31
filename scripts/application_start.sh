bc943754c9ca361f3fc93e63f848adb18707af30
jimkk159 <jimkk159@gmail.com>
2023-06-01 01:00:27 +0800

    feat: add cicd spec

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,8 +3,16 @@
 #give permission
 sudo chmod -R 777 /home/ec2-user/Blog
 
-#to app folder
+#copy the .env
+cp /home/ec2-user/.env /home/ec2-user/Blog/backend/.env
+
+#change work dir
 cd /home/ec2-user/Blog/backend
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 npm install
