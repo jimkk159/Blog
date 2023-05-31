b14d15837e0071203668e4e007bf9945969487a3
jimkk159 <jimkk159@gmail.com>
2023-06-01 03:10:40 +0800

    fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,11 +5,11 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 16
 
-#create our working directory if it does not exist
+#create our working directory if it doesnt exist
 DIR="/home/ec2-user/Blog"
 if [ -d "$DIR" ]; then
-    echo "${DIR} exists"
+  echo "${DIR} exists"
 else
-    echo "Creating ${DIR} directory"
-    mkdir ${DIR}
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
 fi
