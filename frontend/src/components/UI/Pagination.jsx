b09006fe123dc31b9fa33336e210049657641246
jimkk159 <jimkk159@gmail.com>
2023-06-16 11:24:21 +0800

    feat: update pagination

--- frontend/src/components/UI/Pagination.jsx
+++ frontend/src/components/UI/Pagination.jsx
@@ -71,15 +71,18 @@ function Pagination({
   return (
     <ul className={`flex w-full list-none justify-center ${ulCSS}`}>
       <li
-        className={`${liCSS} my-auto flex h-8 items-center rounded-2xl px-1 py-0 text-center leading-snug tracking-wide`}
+        className={
+          `${liCSS} my-auto flex h-8 cursor-pointer items-center rounded-2xl px-1 py-0 text-center leading-snug tracking-wide hover:text-stone-400` +
+          ` ${current === 1 && "pointer-events-none text-stone-700"}`
+        }
         onClick={onFirst}
       >
         <BiFirstPage className={symbolCSS} />
       </li>
       <li
         className={
-          "mx-2 my-auto flex h-8 items-center rounded-2xl px-1 py-0 text-center leading-snug tracking-wide" +
-          ` ${current === 1 && "pointer-events-none"}`
+          "mx-2 my-auto flex h-8 cursor-pointer items-center rounded-2xl px-1 py-0 text-center leading-snug tracking-wide hover:text-stone-400" +
+          ` ${current === 1 && "pointer-events-none text-stone-700"}`
         }
         onClick={onPrev}
       >
@@ -101,26 +104,31 @@ function Pagination({
         return (
           <li
             key={index}
-            className={`${liCSS_2} font-sans mx-2 my-auto flex h-8 cursor-default items-center rounded-2xl bg-transparent px-3 py-0 text-center font-semibold leading-snug tracking-wide ${
-              pageNumber === current && "bg-slate-500"
+            className={`${liCSS_2} mx-2 my-auto flex h-8 w-8 cursor-pointer items-center justify-center rounded-2xl px-3 py-0 text-center font-poppins font-semibold leading-snug tracking-wide hover:text-stone-400 ${
+              pageNumber === current
+                ? "bg-gray-200 bg-opacity-5 text-stone-400 "
+                : "bg-transparent text-stone-500 "
             }`}
             onClick={() => onNavPage(pageNumber)}
           >
-            <p>{pageNumber}</p>
+            {pageNumber}
           </li>
         );
       })}
       <li
         className={
-          "mx-2 my-auto flex h-8 items-center rounded-2xl px-1 py-0 text-center leading-snug tracking-wide" +
-          ` ${current === totalPage && "pointer-events-none"}`
+          "mx-2 my-auto flex h-8 cursor-pointer items-center rounded-2xl px-1 py-0 text-center leading-snug tracking-wide hover:text-stone-400" +
+          ` ${current === totalPage && "pointer-events-none text-stone-700"}`
         }
         onClick={onNext}
       >
         <MdNavigateNext className={symbolCSS} />
       </li>
       <li
-        className="mx-2 my-auto flex h-8 items-center rounded-2xl px-1 py-0 text-center leading-snug tracking-wide"
+        className={
+          "mx-2 my-auto flex h-8 cursor-pointer items-center rounded-2xl px-1 py-0 text-center leading-snug tracking-wide hover:text-stone-400" +
+          `${` ${current === totalPage && "pointer-events-none text-stone-700"}`}`
+        }
         onClick={onLast}
       >
         <BiLastPage className={symbolCSS} />
