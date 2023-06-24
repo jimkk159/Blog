dd811bab6ca23c53bcdfdd15522c114bec4ad790
jimkk159 <jimkk159@gmail.com>
2023-06-25 04:53:10 +0800

    feat: framer-motion cicd test

--- frontend/src/components/UI/SideDrawer.jsx
+++ frontend/src/components/UI/SideDrawer.jsx
@@ -1,25 +1,25 @@
 import ReactDOM from "react-dom";
 import Backdrop from "./Backdrop";
-import { motion } from "framer-motion";
+import { LazyMotion, AnimatePresence, domAnimation, m } from "framer-motion";
 
 function SideDrawer({ show, className, onClick, onCancel, children }) {
   const sideDrawerContent = ReactDOM.createPortal(
-    // <AnimatePresence>
-    <>
-      {show && (
-        <motion.aside
-          transition={{ ease: "easeIn", duration: 0.5 }}
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -100 }}
-          className={`fixed left-0 top-0 z-20 h-full w-[300px] bg-gray-100 shadow-md ${className}`}
-          onClick={onClick}
-        >
-          {children}
-        </motion.aside>
-      )}
-    </>,
-    // </AnimatePresence>,
+    <LazyMotion features={domAnimation}>
+      <AnimatePresence>
+        {show && (
+          <m.aside
+            transition={{ ease: "easeIn", duration: 0.5 }}
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -100 }}
+            className={`fixed left-0 top-0 z-20 h-full w-[300px] bg-gray-100 shadow-md ${className}`}
+            onClick={onClick}
+          >
+            {children}
+          </m.aside>
+        )}
+      </AnimatePresence>
+    </LazyMotion>,
     document.getElementById("drawer")
   );
 
