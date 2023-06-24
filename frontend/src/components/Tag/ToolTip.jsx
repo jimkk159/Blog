358f66a0111f066fb6529d30fbb8de1abf107784
jimkk159 <jimkk159@gmail.com>
2023-06-25 07:29:00 +0800

    fix:  lazy motion for reduce bundle

--- frontend/src/components/Tag/ToolTip.jsx
+++ frontend/src/components/Tag/ToolTip.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState, useEffect } from "react";
 import { useRouteLoaderData } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { motion, AnimatePresence } from "framer-motion";
+import { m, LazyMotion, AnimatePresence, domAnimation } from "framer-motion";
 
 // redux
 import { tagActions } from "../../store/tag-slice";
@@ -54,7 +54,7 @@ function TagsToolTip({
 
   return (
     <div
-      className="relative"
+      className="relative flex"
       onClick={(e) => {
         e.stopPropagation();
         onToggle();
@@ -63,104 +63,104 @@ function TagsToolTip({
       <p className="m-0.5 flex w-[30px] cursor-pointer items-center justify-center rounded-2xl bg-gray-600 px-2 text-[4px] text-base text-gray-50 outline-none">
         +
       </p>
-      {
-        // <AnimatePresence>
-        //   {isEdit && (
-        //     <motion.aside
-        //       transition={{ ease: "easeIn", duration: 0.5 }}
-        //       initial={{ width: 0, height: 0 }}
-        //       animate={{ width: 200, height: 150, }}
-        //       exit={{ width: 0, height: 0 }}
-        //       className="absolute border-2 bottom-8 z-10 overflow-hidden rounded-sm border-gray-200 bg-white p-1.5 shadow ring-1"
-        //     >
-        //       <div className="relative h-full w-full px-1 py-0.5">
-        //         <AwaitWrapper resolve={relation}>
-        //           {(response) => {
-        //             const tags = response?.data?.tags?.data ?? [];
-        //             const choices =
-        //               currentName &&
-        //               tags
-        //                 .filter(
-        //                   (tag) =>
-        //                     tag &&
-        //                     !currentName.includes(
-        //                       tag.name && tag.name.toLowerCase()
-        //                     )
-        //                 )
-        //                 .filter(
-        //                   (tag) =>
-        //                     tag &&
-        //                     tag.name &&
-        //                     (tag.name.startsWith(searchTag) ||
-        //                       tag.name.endsWith(searchTag))
-        //                 );
+      <LazyMotion features={domAnimation}>
+        <AnimatePresence>
+          {isEdit && (
+            <m.div
+              transition={{ ease: "easeIn", duration: 0.5 }}
+              initial={{ width: 0, height: 0 }}
+              animate={{ width: 200, height: 150 }}
+              exit={{ width: 0, height: 0 }}
+              className="absolute bottom-8 z-10 overflow-hidden rounded-sm border-2 border-gray-200 bg-white p-1.5 shadow ring-1"
+            >
+              <div className="relative h-full w-full px-1 py-0.5">
+                <AwaitWrapper resolve={relation}>
+                  {(response) => {
+                    const tags = response?.data?.tags?.data ?? [];
+                    const choices =
+                      currentName &&
+                      tags
+                        .filter(
+                          (tag) =>
+                            tag &&
+                            !currentName.includes(
+                              tag.name && tag.name.toLowerCase()
+                            )
+                        )
+                        .filter(
+                          (tag) =>
+                            tag &&
+                            tag.name &&
+                            (tag.name.startsWith(searchTag) ||
+                              tag.name.endsWith(searchTag))
+                        );
 
-        //             const keyDownHandler = async (e) => {
-        //               if (e.key === "Enter") {
-        //                 const token = authHelper.getAuthToken();
-        //                 const name = e.target.value;
-        //                 let response = await fetch(
-        //                   process.env.REACT_APP_BACKEND_URL + "/api/v1/tags",
-        //                   {
-        //                     method: "POST",
-        //                     headers: {
-        //                       "Content-Type": "application/json",
-        //                       Authorization: "Bearer " + token,
-        //                     },
-        //                     body: JSON.stringify({ name }),
-        //                   }
-        //                 ).catch((err) => {
-        //                   setSubmigErrorMessage("create tag fail");
-        //                 });
-        //                 response = await response.json();
-        //                 const tag = response.data;
-        //                 tags.push(tag);
-        //                 dispatch(tagActions.add({ tag }));
-        //                 setSearchTag("");
-        //                 onClose();
-        //               }
-        //             };
+                    const keyDownHandler = async (e) => {
+                      if (e.key === "Enter") {
+                        const token = authHelper.getAuthToken();
+                        const name = e.target.value;
+                        let response = await fetch(
+                          process.env.REACT_APP_BACKEND_URL + "/api/v1/tags",
+                          {
+                            method: "POST",
+                            headers: {
+                              "Content-Type": "application/json",
+                              Authorization: "Bearer " + token,
+                            },
+                            body: JSON.stringify({ name }),
+                          }
+                        ).catch((err) => {
+                          setSubmigErrorMessage("create tag fail");
+                        });
+                        response = await response.json();
+                        const tag = response.data;
+                        tags.push(tag);
+                        dispatch(tagActions.add({ tag }));
+                        setSearchTag("");
+                        onClose();
+                      }
+                    };
 
-        //             return (
-        //               <>
-        //                 <input
-        //                   ref={inputRef}
-        //                   type="text"
-        //                   className="m-0 w-full border-none pl-1 text-[14px] outline-none"
-        //                   placeholder={"Search/Create..."}
-        //                   value={searchTag}
-        //                   onChange={inputChangeHandler}
-        //                   onKeyDown={keyDownHandler}
-        //                 />
-        //                 <hr className="mb-0 mt-0 w-full border-l-0 border-r-0" />
-        //                 {!isTouched && submigErrorMessage && (
-        //                   <p className="mx-0.5 mb-0 mt-0.5 text-left text-sm text-self-red">
-        //                     {submigErrorMessage}
-        //                   </p>
-        //                 )}
-        //                 <div className={"h-full w-full"}>
-        //                   {choices.map((el, index) => (
-        //                     <p
-        //                       key={index}
-        //                       className="m-0.5 inline-block min-w-[20px] cursor-pointer items-center rounded-2xl bg-gray-600 px-2 text-[4px] text-gray-50 hover:bg-gray-700"
-        //                       onClick={() =>
-        //                         dispatch(tagActions.add({ tag: el }))
-        //                       }
-        //                     >
-        //                       {el.name}
-        //                     </p>
-        //                   ))}
-        //                 </div>
-        //               </>
-        //             );
-        //           }}
-        //         </AwaitWrapper>
-        //       </div>
-        //     </motion.aside>
-        //   )}
-        // </AnimatePresence>
-        <></>
-      }
+                    return (
+                      <>
+                        <input
+                          ref={inputRef}
+                          type="text"
+                          className="m-0 w-full border-none pl-1 text-[14px] outline-none"
+                          placeholder={"Search/Create..."}
+                          value={searchTag}
+                          onChange={inputChangeHandler}
+                          onKeyDown={keyDownHandler}
+                        />
+                        <hr className="mb-0 mt-0 w-full border-l-0 border-r-0" />
+                        {!isTouched && submigErrorMessage && (
+                          <p className="mx-0.5 mb-0 mt-0.5 text-left text-sm text-self-red">
+                            {submigErrorMessage}
+                          </p>
+                        )}
+                        <div className={"h-full w-full"}>
+                          {choices.map((el, index) => (
+                            <p
+                              key={index}
+                              className="m-0.5 inline-block min-w-[20px] cursor-pointer items-center rounded-2xl bg-gray-600 px-2 text-[4px] text-gray-50 hover:bg-gray-700"
+                              onClick={() =>
+                                dispatch(tagActions.add({ tag: el }))
+                              }
+                            >
+                              {el.name}
+                            </p>
+                          ))}
+                        </div>
+                      </>
+                    );
+                  }}
+                </AwaitWrapper>
+              </div>
+            </m.div>
+          )}
+        </AnimatePresence>
+      </LazyMotion>
+      ,
     </div>
   );
 }
--- frontend/src/components/UI/SideDrawer.jsx
+++ frontend/src/components/UI/SideDrawer.jsx
@@ -1,23 +1,25 @@
 import ReactDOM from "react-dom";
 import Backdrop from "./Backdrop";
-import { motion, AnimatePresence } from "framer-motion";
+import { m, LazyMotion, AnimatePresence, domAnimation } from "framer-motion";
 
 function SideDrawer({ show, className, onClick, onCancel, children }) {
   const sideDrawerContent = ReactDOM.createPortal(
-    <AnimatePresence>
-      {show && (
-        <motion.aside
-          transition={{ ease: "easeIn", duration: 0.5 }}
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -100 }}
-          className={`fixed left-0 top-0 z-20 h-full w-[300px] bg-gray-100 shadow-md ${className}`}
-          onClick={onClick}
-        >
-          {children}
-        </motion.aside>
-      )}
-    </AnimatePresence>,
+    <LazyMotion features={domAnimation}>
+      <AnimatePresence>
+        {show && (
+          <m.aside
+            transition={{ ease: "easeIn", duration: 0.5 }}
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -100 }}
+            className={`fixed left-0 top-0 z-20 h-full w-[300px] bg-gray-100 shadow-md ${className}`}
+            onClick={onClick}
+          >
+            {children}
+          </m.aside>
+        )}
+      </AnimatePresence>
+    </LazyMotion>,
     document.getElementById("drawer")
   );
 
