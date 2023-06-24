9363ee8a6fe5c2355b8a6b453e62c8592b19d954
jimkk159 <jimkk159@gmail.com>
2023-06-25 05:39:52 +0800

    fix: cicd test

--- frontend/src/components/UI/SideDrawer.jsx
+++ frontend/src/components/UI/SideDrawer.jsx
@@ -4,20 +4,21 @@ import { motion, AnimatePresence } from "framer-motion";
 
 function SideDrawer({ show, className, onClick, onCancel, children }) {
   const sideDrawerContent = ReactDOM.createPortal(
-    <AnimatePresence>
-      {show && (
-        <motion.aside
-          transition={{ ease: "easeIn", duration: 0.5 }}
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -100 }}
-          className={`fixed left-0 top-0 z-20 h-full w-[300px] bg-gray-100 shadow-md ${className}`}
-          onClick={onClick}
-        >
-          {children}
-        </motion.aside>
-      )}
-    </AnimatePresence>,
+    // <AnimatePresence>
+    //   {show && (
+    //     <motion.aside
+    //       transition={{ ease: "easeIn", duration: 0.5 }}
+    //       initial={{ opacity: 0, x: -100 }}
+    //       animate={{ opacity: 1, x: 0 }}
+    //       exit={{ opacity: 0, x: -100 }}
+    //       className={`fixed left-0 top-0 z-20 h-full w-[300px] bg-gray-100 shadow-md ${className}`}
+    //       onClick={onClick}
+    //     >
+    //       {children}
+    //     </motion.aside>
+    //   )}
+    // </AnimatePresence>,
+    <></>,
     document.getElementById("drawer")
   );
 
